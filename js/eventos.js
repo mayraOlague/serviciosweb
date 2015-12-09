aad6490e073a5c83170e63fb3eb0e43a5ff48dc5
mayraOlague <denisse.verdiales.c@gmail.com>
2015-12-09 11:53:37 -0700

    Terminado Bajas Y Cambios

--- js/eventos.js
+++ js/eventos.js
@@ -3,13 +3,9 @@ var inicioApp =function ()
 {
  var altas = function()
  {
+ 	$("body > section").hide("slow");
  	$("#secAltaAlumnos").show("slow");
  }
- var bajas=function()
- {
- 	$("#secBajaAlumnos").show("slow");
- }
-
  var GuardaAltaAlumno=function()
  {
  	var ncontrol=$("#txtNumControl").val();
@@ -45,12 +41,91 @@ var inicioApp =function ()
 
  	 });
  }
+ var bajas=function()
+ {
+ 	$("body > section").hide("slow");
+ 	$("#secBajaAlumnos").show("slow");
+ }
+ var BajaAlumno=function()
+{
+	
+ 	var ncontrol=$("#txtNumControl3").val();
+ 	
+ 	 var parametros="orden=BajaAlumno"+"&ncontrol="+ncontrol;
+ 	//conexion con PHP
+ 	 $.ajax({
+ 	 	cache:false, // lo desactivamos para  que no se repitan
+ 	 	type:"POST", //que tipo lo vamos a enviar
+ 	 	datatype:"json",
+ 	 	url: "php/funciones.php",
+ 	 	data: parametros,
+ 	 	succes: function(response){// nosva a dar una respuesta si todo salio bien
+ 	 		if(response.respuesta) // tambien se puede poner if(response.respuesta==true)
+ 	 		{
+ 	 			alert("Registro Eliminado");
+ 	 		}
+ 	 		else 
+ 	 		{
+ 	 			alert("No se pudo Eliminar");
+ 	 		}
+ 	 	},
+ 	 	error:function(xhr,ajaxOptions,thrownError){ // si ahi error CONSOLA ES PARA ERROR EN JAVASCRIPT Y NETWORK PARA PHP
+ 	 		console.log("Ocurre un error");
+ 	 	}
+
+
+ 	 });
+ }
+ var cambios=function ()
+ {
+ 	$("body > section").hide("slow");
+ 	// $("#secAltaAlumnos").hide("slow");
+ 	$("#secCambioAlumnos").show("slow");
+  }
+var GuardaCambioAlumno=function(){
+	var ncontrol=$("#txtNumControl2").val();
+ 	var nombres=$("#txtNombres2").val();
+ 	var apellidop=$("#txtApellidoPat2").val();
+ 	var apellidom=$("#txtApellidoMat2").val();
+ 	var carrera=$("#txtClaveCarrera2").val();
+ 	var semestre=$("#txtSemestre2").val();
+ 	var promedio=$("#txtPromedio2").val();
+ 	var estatus=$("#txtEstatus2").val();
+ 	 var parametros="orden=GuardaCambioAlumno"+"&ncontrol="+ncontrol+"&nombres="+nombres+"&apellidop="+apellidop+"&apellidom="+apellidom+"&carrera="+carrera+"&semestre="+semestre+"promedio="+promedio+"&estatus="+estatus;
+ 	//conexion con PHP
+ 	 $.ajax({
+ 	 	cache:false, // lo desactivamos para  que no se repitan
+ 	 	type:"POST", //que tipo lo vamos a enviar
+ 	 	datatype:"json",
+ 	 	url: "php/funciones.php",
+ 	 	data: parametros,
+ 	 	succes: function(response){// nosva a dar una respuesta si todo salio bien
+ 	 		if(response.respuesta) // tambien se puede poner if(response.respuesta==true)
+ 	 		{
+ 	 			alert("Registro Actualizado");
+ 	 		}
+ 	 		else 
+ 	 		{
+ 	 			alert("No se pudo Actualizado");
+ 	 		}
+ 	 	},
+ 	 	error:function(xhr,ajaxOptions,thrownError){ // si ahi error CONSOLA ES PARA ERROR EN JAVASCRIPT Y NETWORK PARA PHP
+ 	 		console.log("Ocurre un error");
+ 	 	}
+
+
+ 	 });
+}
+
  //cuando damos click en el boton btnAltas
  //Se dispara la funcion: altas
+ $("#btnCambios").on("click",cambios);
  $("#btnBajas").on("click",bajas);
  $("#btnAltas").on("click",altas);
  $("#frmAltaAlumnos").on("submit",GuardaAltaAlumno);
- $("#frmBajaAlumnos").on("submit",BajaAlumno)
-}
+ $("#frmBajaAlumno").on("submit",BajaAlumno);
+ $("#frmCambioAlumnos").on("submit",GuardaCambioAlumno);
 //al estar listo el documento dispara la funcion inicial.
+
+}
 $(document).on("ready",inicioApp);
--- php/funciones.php
+++ php/funciones.php
@@ -24,38 +24,64 @@ function GuardaAltaAlumno()
 	print json_encode($salidaJSON);
 }
 
-$orden= $_POST["orden"];
-switch ($orden) {
-	case 'GuardaAltaAlumno':
-		GuardaAltaAlumno();
-		break;
-	
-	default:
-		# code...
-		break;
-}
+
 function BajaAlumno()
 {
 	$nco ="'".$_POST["ncontrol"]."'";
 	$con = mysql_connect("localhost","root","");
 	mysql_select_db("pw10");
-	$consulta = sprintf("delete from alumnos where ncontrol=%s",$ncontrol);
+	$consulta = sprintf("delete from alumnos where ncontrol=%s",$nco);
 		mysql_query($consulta);
-		if(mysql_affected_rows() > 0)
-			print "Registro eliminado";
-		else
-			print "El registro no se pudo eliminar";
+		$respuesta= false; //enviar al JS
+		if (mysql_affected_rows()>0)
+	{
+		$respuesta= true;
+	} 
+	//enviamos los datos al JavaScript
+	$salidaJSON= array('respuesta' => $respuesta);
+	print json_encode($salidaJSON);
+}
+	
 
-	}
+
+function GuardaCambioAlumno()
+{
+	$nco ="'".$_POST["ncontrol"]."'";
+	$nom ="'".$_POST["nombres"]."'";
+	$app ="'".$_POST["apellidop"]."'";
+	$apm ="'".$_POST["apellidom"]."'";
+	$car = $_POST["carrera"];
+	$sem = $_POST["semestre"];
+	$pro = $_POST["promedio"];
+	$est ="'".$_POST["estatus"]."'";
+	$con = mysql_connect("localhost","root","");
+	mysql_select_db("pw10");
+	$consulta=sprintf("update alumnos set nombres=%s,apellidopaterno=%s,apellidomaterno=%s,clavecarrera=%d,semestre=%d,promedio=%d,estatus=%s where ncontrol=%s",$nom,$app,$apm,$car,$sem,$pro,$est,$nco);
+	mysql_query($consulta);
+
+	$respuesta= false; //enviar al JS
+	if (mysql_affected_rows()>0)
+	{
+		$respuesta= true;
+	} 
+	//enviamos los datos al JavaScript
+	$salidaJSON= array('respuesta' => $respuesta);
+	print json_encode($salidaJSON);
+}
 $orden= $_POST["orden"];
 switch ($orden) {
+	case 'GuardaAltaAlumno':
+		GuardaAltaAlumno();
+		break;
 	case 'BajaAlumno':
 		BajaAlumno();
 		break;
-	
+	case 'GuardaCambioAlumno':
+	GuardaCambioAlumno();
+	break;
+
 	default:
 		# code...
 		break;
-
 }
 ?>
